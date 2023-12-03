42151ff66e05b14e4abb06a1abc6aaf723c87d50
nw <66349812+W1nOfGood@users.noreply.github.com>
2023-12-03 23:22:23 +0700

    lp test

--- src/webui.py
+++ src/webui.py
@@ -168,7 +168,7 @@ if __name__ == '__main__':
 
     with gr.Blocks(title='KPOPEASYGUI 🔊',theme=gr.themes.Base(primary_hue="rose", secondary_hue="pink", neutral_hue="slate")) as app:
 
-        gr.HTML("<img  src='src/lp.gif' alt='This is supposed to be a logo but if it breaks it will show this message so haha... tell .nww he need to fix this'>")
+        gr.HTML("<img  src='file/images/lp.gif' alt='This is supposed to be a logo but if it breaks it will show this message so haha... tell .nww he need to fix this'>")
 
 
         # main tab
